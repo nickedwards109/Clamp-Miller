4400af9ac4c87cbd58dd4fbf7c263ad0a5bb2b2d
edwardsCNC <nicholas.lee.edwards@gmail.com>
2015-08-07 21:30:58 -0600

    Initial working version. Generates a CNC milling program for machining a part for a milling clamp set. The machined part has a hole for a threaded height adjustment bolt, a slot for a fastening bolt, and a rounded outside profile.

---
+++ ClampMaker.rb
@@ -0,0 +1,251 @@
+class ClampMaker
+
+
+
+	def initialize(length, width, material_thickness, hole_diameter, slot_to_hole_distance, slot_width, slot_to_end_of_clamp_distance, tool_radius, xy_feedrate, z_feedrate, axial_depth_of_cut, safe_z_height)
+		
+		@length = length
+		@width = width
+		@material_thickness = material_thickness
+		@hole_diameter = hole_diameter
+		@slot_to_hole_distance = slot_to_hole_distance
+		@slot_width = slot_width
+		@slot_to_end_of_clamp_distance = slot_to_end_of_clamp_distance
+		@tool_radius = tool_radius
+		@xy_feedrate = xy_feedrate
+		@z_feedrate = z_feedrate
+		@axial_depth_of_cut = axial_depth_of_cut
+		@safe_z_height = safe_z_height
+
+		@large_profile_radius = width/2
+		@small_profile_radius = width/4
+		@half_width = width/2
+
+	end
+
+
+
+	def create_toolpaths
+
+		self.create_hole_toolpath
+		self.create_slot_toolpath
+		self.create_outer_profile_toolpath
+
+	end
+
+
+
+		def create_hole_toolpath
+
+			#rapid feed to the safe z height to prepare for XY motion
+			puts "G0Z#{@safe_z_height.round(3)}"
+
+			#rapid feed to the XY location of the hole
+			puts "G0X#{@large_profile_radius.round(3)}Y#{@length.round(3) - @large_profile_radius.round(3)}"
+
+			#rapid feed/plunge to one tool radius above Z=0
+			puts "G0Z#{@tool_radius}"
+			
+			#specify a variable to store the amount of material that is not yet machined
+			remaining_material = @material_thickness
+
+				#create multiple successive profile passes while incrementing the axial depth of cut
+				while (remaining_material + @tool_radius) > 0
+
+					#feed/plunge down in Z by an increment of one axial depth of cut
+					puts "G91"
+					puts "G1Z-#{@axial_depth_of_cut.round(3)}F#{@z_feedrate}"
+					puts "G90"
+
+					#create an XY toolpath for machining the outside profile
+					self.create_XY_hole_profile_toolpath
+
+					#there is now one axial depth of cut less of material
+					remaining_material = remaining_material - @axial_depth_of_cut
+
+				end
+
+		end
+
+				def create_XY_hole_profile_toolpath
+
+					#The tool is plunged into the material by one axial depth of cut. 
+					#the tool has not yet made any other circular passes to machine the hole's inner profile. 
+
+					#set a variable to represent remaining inner profile hole stock in the XY plane (as a diameter)
+					remaining_hole_stock = @hole_diameter - 2*@tool_radius
+
+					#set a counting variable for use within the circular toolpath generation loop
+					i = 1
+
+					#rough out the hole diameter until there is less than one tool radius of stock remaining on each opposing side
+					while (remaining_hole_stock/2) > @tool_radius
+
+						#feed into the material in the +X direction by an amount of one tool radius in order to prepare for a circular pass
+						puts "G1X#{(@large_profile_radius + i*@tool_radius).round(3)}F#{@xy_feedrate}"
+
+						#make a single climb milling circular pass. the final XY values are equivalent to the XY values before the G3 command, creating a complete circle centered about X = @large_profile_radius and Y = length - @large_profile_radius
+						puts "G3X#{(@large_profile_radius + i*@tool_radius).round(3)}Y#{(@length - @large_profile_radius).round(3)}I-#{i*@tool_radius.round(3)}J0.0F#{@xy_feedrate}"
+
+						remaining_hole_stock = remaining_hole_stock - 2*@tool_radius
+
+						i = i + 1
+
+					end
+
+					#move to furthest X position in the hole to prepare for machining the final diameter of the hole
+					puts "G1X#{(@large_profile_radius - 0.5*@hole_diameter + @tool_radius).round(3)}F#{@xy_feedrate}"
+
+					#create the final pass to machine the final the diameter of the hole
+					puts "G3X#{(@large_profile_radius - 0.5*@hole_diameter + @tool_radius).round(3)}Y#{(@length - @large_profile_radius).round(3)}I#{(0.5*@hole_diameter - @tool_radius).round(3)}J0.0F#{@xy_feedrate}"
+
+					#return to the XY center of the hole to prepare for another axial depth of cut
+					puts "G0X#{@large_profile_radius.round(3)}Y#{@length.round(3) - @large_profile_radius.round(3)}"
+
+				end
+
+
+
+		def create_slot_toolpath
+
+			#rapid feed to the safe z height to prepare for XY motion
+			puts "G0Z#{@safe_z_height.round(3)}"
+
+			#rapid feed to the XY coordinates at the radius center at the slot top
+			puts "G0X#{@half_width.round(3)}Y#{(@length - @large_profile_radius - @slot_to_hole_distance).round(3)}"
+
+			#rapid feed/plunge to one tool radius above Z=0
+			puts "G0Z#{@tool_radius}"
+			
+			#specify a variable to store the amount of material that is not yet machined
+			remaining_material = @material_thickness
+
+				#create multiple successive profile passes while incrementing the axial depth of cut
+				while (remaining_material + @tool_radius) > 0
+
+					#feed/plunge down in Z by an increment of one axial depth of cut
+					puts "G91"
+					puts "G1Z-#{@axial_depth_of_cut.round(3)}F#{@z_feedrate}"
+					puts "G90"
+
+					#create an XY toolpath for machining the outside profile
+					self.create_XY_slot_profile_toolpath
+
+					remaining_material = remaining_material - @axial_depth_of_cut
+
+				end
+
+		end
+
+
+				def create_XY_slot_profile_toolpath
+					#tool position is at the end of the slot near the hole
+
+					#feed to the Y position at the opposite end of the slot
+					puts "G1Y#{@slot_to_end_of_clamp_distance.round(3)}F#{@xy_feedrate}"
+
+					#set a variable to represent total slot stock remaining on both opposing sides
+					remaining_slot_stock = @slot_width - 2*@tool_radius
+
+					#set a counting variable for use within the circular toolpath generation loop
+					i = 1
+
+					#rough out the inner slot profile until there is less than one tool radius of stock remaining on both opposing sides
+					while (remaining_slot_stock/2) > @tool_radius
+
+						#feed in the +X direction by one tool radius to prepare for an inner profile climb milling cut
+						puts "G1X#{(@half_width + i*@tool_radius).round(3)}F#{@xy_feedrate}F#{@xy_feedrate}"
+
+						#make a linear interpolation climb milling pass in the +Y direction
+						puts "G1Y#{(@length - @large_profile_radius - @slot_to_hole_distance).round(3)}F#{@xy_feedrate}"
+
+						#make a counter-clockwise circular interpolation pass about the top radius of the slot
+						puts "G3X#{(@half_width - i*@tool_radius).round(3)}I-#{(i*@tool_radius).round(3)}J0.0F#{@xy_feedrate}"
+
+						#make a linear interpolation climb milling pass in the -Y direction
+						puts "G1Y#{@slot_to_end_of_clamp_distance.round(3)}F#{@xy_feedrate}"
+
+						#make a counter-clockwise circular interpolation pass about the bottom radius of the slot
+						puts "G3X#{(@half_width + i*@tool_radius).round(3)}I#{(i*@tool_radius).round(3)}F#{@xy_feedrate}" 
+
+						remaining_slot_stock = remaining_slot_stock - 2*@tool_radius
+
+						i = i + 1
+
+					end
+
+					#create the final passes to machine the final inner profile of the slot
+					#feed in the +X direction to the position where the tool edge is at the final profile of the slot
+					puts "G1X#{(@half_width + @slot_width/2 - @tool_radius).round(3)}F#{@xy_feedrate}"
+
+					#make the final linear interpolation climb milling pass in the +Y direction
+					puts "G1Y#{(@length - @large_profile_radius - @slot_to_hole_distance).round(3)}F#{@xy_feedrate}"
+
+					#make the final counter-clockwise circular interpolation pass about the top radius of the slot
+					puts "G3X#{(@half_width - @slot_width/2 + @tool_radius).round(3)}I-#{(@slot_width/2 - @tool_radius).round(3)}J0.0F#{@xy_feedrate}"
+
+					#make the final linear interpolation climb milling pass in the -Y direction
+					puts "G1Y#{@slot_to_end_of_clamp_distance.round(3)}F#{@xy_feedrate}"
+
+					#make the final counter-clockwise circular interpolation pass about the bottom radius of the slot
+					puts "G3X#{(@half_width + @slot_width/2 - @tool_radius).round(3)}I#{(@slot_width/2 - @tool_radius).round(3)}F#{@xy_feedrate}" 
+				end
+
+
+
+
+		def create_outer_profile_toolpath
+
+			#rapid feed to the safe z height to prepare for XY motion
+			puts "G0Z#{@safe_z_height.round(3)}"
+
+			#rapid feed to the XY coordinates for a Z plunge at the top of the lower right radius of the clamp
+			puts "G0X#{(@width + @tool_radius).round(3)}Y#{(@small_profile_radius + @tool_radius).round(3)}"
+
+			#rapid feed/plunge to one tool radius above Z=0
+			puts "G0Z#{@tool_radius}"
+			
+			#specify a variable to store the amount of material that is not yet machined
+			remaining_material = @material_thickness
+
+				#create multiple successive profile passes while incrementing the axial depth of cut
+				while (remaining_material + @tool_radius) > 0
+
+					#feed/plunge down in Z by an increment of one axial depth of cut
+					puts "G91"
+					puts "G1Z-#{@axial_depth_of_cut.round(3)}F#{@z_feedrate}"
+					puts "G90"
+
+					#create an XY toolpath for machining the outside profile
+					self.create_XY_outer_profile_toolpath
+
+					remaining_material = remaining_material - @axial_depth_of_cut
+
+				end
+
+		end
+
+				def create_XY_outer_profile_toolpath
+
+					#counter-clockwise circular interpolation feed to machine the lower right profile radius
+					puts "G2X#{(@width - @small_profile_radius).round(3)}Y-#{@tool_radius.round(3)}I-#{@small_profile_radius + @tool_radius}J0.0F#{@xy_feedrate}"
+
+					#linear interpolation feed to an X location of one small profile radius from the left side
+					puts "G1X#{@small_profile_radius.round(3)}F#{@xy_feedrate}"
+
+					#counter-clockwise circular interpolation feed to machine the lower left profile radius
+					puts "G2X-#{@tool_radius.round(3)}Y#{@small_profile_radius.round(3)}I0.0J#{(@small_profile_radius + @tool_radius).round(3)}F#{@xy_feedrate}"
+
+					#linear interpolation feed to a Y location of the clamp length minus the profile radius
+					puts "G1Y#{(@length - @large_profile_radius).round(3)}F#{@xy_feedrate}"
+
+					#counter-clockwise circular interpolation feed to machine the large profile radius
+					puts "G2X#{(@width + @tool_radius).round(3)}Y#{(@length - @large_profile_radius).round(3)}I#{(@large_profile_radius + @tool_radius).round(3)}J0.0F#{@xy_feedrate}"
+
+					#linear interpolation feed to the starting position
+					puts "G1X#{(@width + @tool_radius).round(3)}Y#{(@small_profile_radius + @tool_radius).round(3)}F#{@xy_feedrate}"
+
+				end
+
+
+end
---
+++ Mill-Clamp.rb
@@ -0,0 +1,50 @@
+require './ClampMaker.rb'
+require './CNCHelper.rb'
+
+puts "This program creates a toolpath for machining an outer profile, a hole, and a slot to create a clamp piece."
+
+puts "Set X=0 and Y=0 on the workpiece such that the Cartesian quadrant within X>0 and Y>0 is solid workpiece material."
+
+puts "Use a slot drill (an end mill that can plunge in Z)."
+
+puts "What is the length of the clamp?"
+	 length = Float(gets)
+
+puts "What is the width of the clamp?"
+	 width = Float(gets)
+
+puts "What is the thickness of the material stock?"
+	 material_thickness = Float(gets)
+
+puts "There will be a hole for housing the threaded female piece for holding the height adjustment screw. What is the diameter of this height adjustment screw hole?"
+	 hole_diameter = Float(gets)
+
+puts "There will be a slot for the fastening screw. One end of the slot (at the center of the slot end's radius) will have a certain distance to the height adjustment screw hole. What is this distance?"
+	 slot_to_hole_distance = Float(gets)
+
+puts "The opposite end of the slot will have a certain distance to the end of the clamp. What is this distance?"
+	 slot_to_end_of_clamp_distance = Float(gets)
+
+puts "How wide is the slot?"
+	 slot_width = Float(gets)
+
+puts "What is the tool radius?"
+	 tool_radius = Float(gets)
+
+puts "What is the XY feedrate? This is the feedrate used for end-milling."
+	 xy_feedrate = Float(gets)
+
+puts "What is the Z feedrate? This is the feedrate used for plunging to the next depth of cut."
+	 z_feedrate = Float(gets)
+
+puts "What is the axial depth of cut?"
+	 axial_depth_of_cut = Float(gets)
+
+puts "What is a safe Z height that the tool will retract to before rapid feed to a new XY location?"
+	 safe_z_height = Float(gets)
+
+puts "OK. Here is your CNC program:"
+puts ""
+
+clampmaker = ClampMaker.new(length, width, material_thickness, hole_diameter, slot_to_hole_distance, slot_width, slot_to_end_of_clamp_distance, tool_radius, xy_feedrate, z_feedrate, axial_depth_of_cut, safe_z_height)
+clampmaker.create_toolpaths
