2632398901979601afbe0ce048c4700d09b030c5
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-08-03 20:38:10 -0600

    Improve comments in spec for ClampMaker#create_XY_hole_profile_toolpath
    Moved comments about CNC code into the matcher where the CNC code is displayed, which is consistent with the format of the other test in the suite. Fixed a geometry error in one of the comments, and confirmed that this error was not in the test itself.

--- spec/clampmaker_spec.rb
+++ spec/clampmaker_spec.rb
@@ -7,14 +7,10 @@ RSpec.describe ClampMaker do
   context "length: 3.0, width: 1.5, material_thickness: 0.5, xy_feedrate: 15.0, z_feedrate: 10.0, axial_depth_of_cut: 0.15" do
     let(:clampmaker) { ClampMaker.new(3.0, 1.5, 0.5, 15.0, 10.0, 0.15) }
 
-      # In this context, CNC code for a proper hole XY profile has the following properties:
-      # The hole is machined with two passes, starting with the cutting tool centered initially at X=0.813 and subsequently at X=0.85
-      # The hole is centered at a Y location of Y=2.25
-      # The hole is centered at an X location of X=0.75, implied by the X positions plus the I offset values
       it "generates CNC code for machining a proper hole XY profile" do
         expect {clampmaker.create_XY_hole_profile_toolpath}.to output(/
-                                                                      G1X0.85Y2.25F15.0\n # Feed to the X position for cutting the XY hole profile
-                                                                      G3X0.85Y2.25I-0.1J0.0F15.0\n  # Cut a circle offset by -0.1 along X and 0.0 along Y
+                                                                      G1X0.85Y2.25F15.0\n # Feed to the XY position for starting to cut the hole, at X0.85 and Y2.25
+                                                                      G3X0.85Y2.25I-0.1J0.0F15.0\n  # Cut a circle centered at X0.75 and Y2.25, offset by -0.1 along X and 0.0 along Y
                                                                       /x).to_stdout
       end
       
