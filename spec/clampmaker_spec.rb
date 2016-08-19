bee317e0a60e6207e65ca17c2f95b64c45fef9f2
edwardsCNC <nicholas.lee.edwards@gmail.com>
2016-08-18 18:21:19 -0600

    Red spec for ClampMaker#create_outer_profile_toolpath

--- spec/clampmaker_spec.rb
+++ spec/clampmaker_spec.rb
@@ -80,7 +80,7 @@ RSpec.describe ClampMaker do
                                                         G90\n  # Set absolute mode
                                                         G0X0.75Y1.75\n  # Rapid feed to the XY position of the slot end diameter center
                                                         G91\n  # Set incremental mode
-                                                        G0Z-0.063\n  # Rapid feed to tone tool radius above the material
+                                                        G0Z-0.063\n  # Rapid feed to one tool radius above the material
                                                         G1Z-0.263F10.0\n  # Feed one depth of cut into the material
                                                         G90\n  # Set absolute mode
                                                         G1Y0.5F15.0\n  # Feed through the X middle of the slot to the Y position at the lower end of the slot
@@ -95,7 +95,7 @@ RSpec.describe ClampMaker do
                                                         G90\n  # Set absolute mode
                                                         G0X0.75Y1.75\n  # Rapid feed to the XY position of the slot end diameter center
                                                         G91\n  # Set incremental mode
-                                                        G0Z-0.063\n  # Rapid feed to tone tool radius above the material
+                                                        G0Z-0.063\n  # Rapid feed to one tool radius above the material
                                                         G1Z-0.263F10.0\n  # Feed one depth of cut into the material
                                                         G90\n  # Set absolute mode
                                                         G1Y0.5F15.0\n  # Feed through the X middle of the slot to the Y position at the lower end of the slot
@@ -106,6 +106,40 @@ RSpec.describe ClampMaker do
                                                         G3X0.788I0.038J0.0F15.0\n  # Make a counter-clockwise circular interpolation pass about the lower radius of the slot
                                                         G0Z0.0\n  # Return to Z zero
                                                         /x).to_stdout
+    end
+
+    it "generates CNC code for machining the outer profile all the way through the material in the Z axis" do
+      expect {clampmaker.create_outer_profile_toolpath}.to output(/
+                                                                 # First cutting pass
+                                                                 G91\n  # Set incremental mode
+                                                                 G0Z0.125\n  # Rapid feed up in Z by one tool diameter
+                                                                 G90\n  # Set absolute mode
+                                                                 G0X1.563Y0.375\n  # Rapid feed to the XY coordinates for a Z plunge at the top of the lower right radius of the clamp
+                                                                 G91\n  # Set incremental mode
+                                                                 G0Z-0.063\n  # Rapid feed to one tool radius above the material
+                                                                 G1Z-0.263F10.0\n  # Feed one depth of cut into the material
+                                                                 G90\n  # Set absolute mode
+                                                                 G2X1.125Y-0.063I-0.438J0.0F15.0\n  # Counter-clockwise circular interpolation feed to machine the lower right profile radius
+                                                                 G1X0.375F15.0\n  # Feed to an X location of one small profile radius from the left side
+                                                                 G2X-0.063Y0.375I0.0J0.438F15.0\n # Counter-clockwise circular interpolation feed to machine the lower left profile radius
+                                                                 G1Y2.25F15.0\n # Feed to a Y location of the clamp length minus the large profile radius
+                                                                 G2X1.563Y2.25I0.813J0.0F15.0\n # Counter-clockwise circular interpolation feed to machine the large profile radius
+                                                                 G1X1.563Y0.375F15.0\n # Feed to the starting position
+                                                                 # Second cutting pass. Since the cutting tool ended its last motion at the start position...
+                                                                 # ...it can immediately feed one axial depth of cut into the material, without first approaching from a safe Z position.
+                                                                 G91\n  # Set incremental mode
+                                                                 G1Z-0.2F10.0\n  # Feed one depth of cut into the material
+                                                                 G90\n  # Set absolute mode 
+                                                                 G2X1.125Y-0.063I-0.438J0.0F15.0\n  # Counter-clockwise circular interpolation feed to machine the lower right profile radius
+                                                                 G1X0.375F15.0\n  # Feed to an X location of one small profile radius from the left side
+                                                                 G2X-0.063Y0.375I0.0J0.438F15.0\n # Counter-clockwise circular interpolation feed to machine the lower left profile radius
+                                                                 G1Y2.25F15.0\n # Feed to a Y location of the clamp length minus the large profile radius
+                                                                 G2X1.563Y2.25I0.813J0.0F15.0\n # Counter-clockwise circular interpolation feed to machine the large profile radius
+                                                                 G1X1.563Y0.375F15.0\n # Feed to the starting position
+                                                                 G0Z0.125\n  # Rapid feed to a safe Z position
+                                                                 G0X0.0Y0.0\n  # Rapid feed to XY origin
+                                                                 G1Z0.0\n  # Rapid feed to Z origin
+                                                                 /x).to_stdout
     end
       
   end
