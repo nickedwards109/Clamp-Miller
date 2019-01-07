90f253e2274af1b085c9d21494491b9cfd6f374c
nickedwards109 <nicholas.lee.edwards@gmail.com>
2019-01-07 16:16:38 -0500

    explain to users what to do with the CNC program
    this commit adds some text in the UI above the CNC program that explains to users how to preview it and where to find guidelines for running it in a CNC router.

--- lib/GCodeTemplate.rb
+++ lib/GCodeTemplate.rb
@@ -2,7 +2,8 @@ class GCodeTemplate
   attr_reader :template
 
   def initialize
-    @template = ''
+    explanatory_message = '<span>Here is your CNC program! You can copy it and paste it <a href="https://nraynaud.github.io/webgcode/">here</a> to preview it. Please take a look at the README in the <a href="https://github.com/nickedwards109/Clamp-Miller">source code</a> for guidelines on setting up your CNC router.</span><br/><br/>'
+    @template = explanatory_message
   end
 
   def add_gcode_block(gcode)
